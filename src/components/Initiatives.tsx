49ba1531548583327f34845ee5c111aba4617aa8
Jallah <j.sumbo@alustudent.com>
2026-05-29 13:07:42 +0000

    Fix initiatives grid: single column on mobile, 2-col tablet, 3-col desktop

--- src/components/Initiatives.tsx
+++ src/components/Initiatives.tsx
@@ -31,7 +31,7 @@ export default function Initiatives() {
         </div>
 
         {/* Cards grid */}
-        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-5 lg:gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 sm:gap-5 lg:gap-6">
           {INITIATIVES.map((initiative) => (
             <div
               key={initiative.slug}
